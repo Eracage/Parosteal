99e3a702bc596983594428a51e65b813f38ec2a6
Esko Haila <eracage@gmail.com>
2014-11-22 02:22:47 +0200

    added player movement

--- vs2013/Parosteal/source/Player.cpp
+++ vs2013/Parosteal/source/Player.cpp
@@ -3,6 +3,51 @@
 
 using namespace uth;
 
+pmath::Vec2 control()
+{
+	pmath::Vec2 retVal;
+
+	if (uthInput.Keyboard.IsKeyDown(Keyboard::Numpad1))
+	{
+		retVal.x -= 1;
+		retVal.y += 1;
+	}
+	if (uthInput.Keyboard.IsKeyDown(Keyboard::Numpad2) || uthInput.Keyboard.IsKeyDown(Keyboard::Down))
+	{
+		retVal.y += 1;
+	}
+	if (uthInput.Keyboard.IsKeyDown(Keyboard::Numpad3))
+	{
+		retVal.x += 1;
+		retVal.y += 1;
+	}
+	if (uthInput.Keyboard.IsKeyDown(Keyboard::Numpad4) || uthInput.Keyboard.IsKeyDown(Keyboard::Left))
+	{
+		retVal.x -= 1;
+	}
+	if (uthInput.Keyboard.IsKeyDown(Keyboard::Numpad6) || uthInput.Keyboard.IsKeyDown(Keyboard::Right))
+	{
+		retVal.x += 1;
+	}
+	if (uthInput.Keyboard.IsKeyDown(Keyboard::Numpad7))
+	{
+		retVal.x -= 1;
+		retVal.y -= 1;
+	}
+	if (uthInput.Keyboard.IsKeyDown(Keyboard::Numpad8) || uthInput.Keyboard.IsKeyDown(Keyboard::Up))
+	{
+		retVal.y -= 1;
+	}
+	if (uthInput.Keyboard.IsKeyDown(Keyboard::Numpad9))
+	{
+		retVal.y -= 1;
+		retVal.x += 1;
+	}
+	if (retVal != pmath::Vec2())
+		return retVal.normalize();
+	return retVal;
+}
+
 Player::Player()
 	: GameObject("Player")
 {
@@ -20,8 +65,12 @@ Player::Player()
 
 void Player::update(float dt)
 {
-	pmath::Vec2 tipPos = m_tip->transform.GetPosition();
-	m_tip->transform.Move((tipPos + uth::Randomizer::InsideCircle((1 + tipPos.length())/10)) * dt);
+	const pmath::Vec2 tipPos = m_tip->transform.GetPosition();
+	m_tip->transform.Move((/*tipPos +*/ uth::Randomizer::InsideCircle((5 + pow(tipPos.length(),0.1))/10)) * dt);
 
-	transform.Move(pmath::Mat2::createRotation(90) * tipPos * dt);
+	m_tip->transform.Move(control() * dt * 80);
+
+	pmath::Vec2 movement = pmath::Mat2::createRotation(90) * m_tip->transform.GetPosition() * sqrt(m_tip->transform.GetPosition().length()) * dt * 0.8f;
+	m_tip->transform.Move(movement);
+	transform.Move(-movement * 0.2);
 }

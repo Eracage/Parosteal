33f708d454af1e64541c4f503d98d67d6c2ffb94
Esko Haila <eracage@gmail.com>
2014-11-23 20:45:09 +0200

    changed to window project, added icon to exe and window

--- vs2013/Parosteal/source/main.cpp
+++ vs2013/Parosteal/source/main.cpp
@@ -3,6 +3,33 @@
 #define NEWSCENEFUNC
 #include <Scenes.hpp>
 
+int WINAPI WinMain(HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow)
+{
+	srand(time(0));
+
+	uthSceneM.registerNewSceneFunc(NewSceneFunc, COUNT);
+
+	uthEngine.Init();
+
+	uthEngine.GetWindow().GetCamera().SetSize(1280, 720);
+
+	while (uthEngine.Running())
+	{
+		if (uthInput.Keyboard.IsKeyPressed(uth::Keyboard::Escape))
+			uthEngine.Exit();
+
+		uthEngine.Update();
+
+		uthEngine.Draw();
+	}
+
+	return 0;
+}
+
+
 int main()
 {
 	srand(time(0));
